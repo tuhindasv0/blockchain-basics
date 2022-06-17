10c7f617f4f866639b77a9878be8923578ea8756
tuhindasv0 <tuhindasv0@gmail.com>
2022-06-17 19:34:46 +0530

    etherjs added

---
+++ 04_Ether_JS/SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Storage {
+    uint256 number;
+
+    struct People {
+        string name;
+        uint256 favouriteNumber;
+    }
+
+    People[] public persons;
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    mapping(string => uint256) public nameToNumber;
+
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+
+    function addPerson(string memory _name, uint256 _number) public {
+        persons.push(People(_name, _number));
+        nameToNumber[_name] = _number;
+    }
+}
