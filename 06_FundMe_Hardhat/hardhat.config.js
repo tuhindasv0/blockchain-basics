3b778408a15f53d3e012583f76a372088eac1a61
tuhindasv0 <tuhindasv0@gmail.com>
2022-06-21 00:06:14 +0530

    add fund me code

--- 06_FundMe_Hardhat/hardhat.config.js
+++ 06_FundMe_Hardhat/hardhat.config.js
@@ -22,9 +22,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.7",
   networks: {
-,
+
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
